7c84014449d0051f62074d290e762a1aeb29c53f
Alejandro Rosero <alejandro.rosero@klarna.com>
2022-12-01 14:08:52 +0100

    AOC day1. Add debug log and fail if test breaks.

--- 2022/01/star1.py
+++ 2022/01/star1.py
@@ -1,19 +1,29 @@
+def debug(log_message):
+    import datetime;
+    debug_enabled = False
+    if debug_enabled:
+        print(f"[{datetime.datetime.now()}] - [DEBUG] - {log_message}")
+
 def getMaxCalories(file_name):
     max_value = 0
     current = 0
     with open(file_name) as f:
         for index, line in enumerate(f):
-            print(line.strip())
+            debug(line.strip())
             if(line == "\n"):
                 if current > max_value:
                     max_value = current
-                    print(f"max: {max_value}")
+                    debug(f"max: {max_value}")
                 current = 0
             else:
                 current = current + int(line)
-                print(f"current: {current}")
-                print(f"max: {max_value}")
+                debug(f"current: {current}")
+                debug(f"max: {max_value}")
     return max_value
 
-print(getMaxCalories("test_input"))
-print(getMaxCalories("input"))
+test_result = getMaxCalories("test_input")
+expected_result = 24000
+if test_result != expected_result:
+    raise Exception(f"TEST FAILED: Expected value: {expected_result}, got: {test_result}")
+
+print(f"Result: {getMaxCalories('input')}")
--- 2022/01/star2.py
+++ 2022/01/star2.py
@@ -1,29 +1,47 @@
+def debug(log_message):
+    import datetime;
+    debug_enabled = False
+    if debug_enabled:
+        print(f"[{datetime.datetime.now()}] - [DEBUG] - {log_message}")
+
+def sort_top3(current, top_3):
+    if current > top_3[0]:
+        top_3[2] = top_3[1]
+        top_3[1] = top_3[0]
+        top_3[0] = current
+        debug(f"top_3[0]: {top_3[0]}")
+
+    elif current > top_3[1]:
+        top_3[2] = top_3[1]
+        top_3[1] = current
+        debug(f"top_3[1]: {top_3[1]}")
+    elif current > top_3[2]:
+        top_3[2] = current
+        debug(f"top_3[2]: {top_3[2]}")
+    return top_3
+
 def getMaxCalories(file_name):
     top_3 = [0,0,0]
     current = 0
     with open(file_name) as f:
         for index, line in enumerate(f):
-            print(line.strip())
+            debug(line.strip())
             if(line == "\n"):
-                if current > top_3[0]:
-                    top_3[2] = top_3[1]
-                    top_3[1] = top_3[0]
-                    top_3[0] = current
-                    print(f"top_3[0]: {top_3[0]}")
-
-                elif current > top_3[1]:
-                    top_3[2] = top_3[1]
-                    top_3[1] = current
-                    print(f"top_3[1]: {top_3[1]}")
-                elif current > top_3[2]:
-                    top_3[2] = current
-                    print(f"top_3[2]: {top_3[2]}")
+                top_3 = sort_top3(current, top_3)
                 current = 0
             else:
                 current = current + int(line)
-                print(f"current: {current}")
-                print(f"max: {top_3}")
+                debug(f"current: {current}")
+                debug(f"max: {top_3}")
+
+
+    top_3 = sort_top3(current, top_3)
     return sum(top_3)
 
-print(getMaxCalories("test_input"))
-print(getMaxCalories("input"))
+test_result = getMaxCalories("test_input")
+expected_result = 45000
+if test_result != expected_result:
+    raise Exception(f"TEST FAILED: Expected value: {expected_result}, got: {test_result}")
+
+
+print(f"Result: {getMaxCalories('input')}")
