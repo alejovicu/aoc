7896276cc26df394ff47e792499019b8ecb3b3c0
Alejandro Rosero Vicuña <alejandroroserov@gmail.com>
2022-12-10 10:32:46 +0100

    day5: star2

--- 2022/05/star2.py
+++ 2022/05/star2.py
@@ -1,41 +1,121 @@
+from collections import deque
+
 def debug(log_message):
     import datetime;
+    # debug_enabled = True
     debug_enabled = False
     if debug_enabled:
         print(f"[{datetime.datetime.now()}] - [DEBUG] - {log_message}")
 
-def f1():
-    # TODO
-    return 0
+def process_stack_lines(stack_str):
+    tmp_matrix = []
+    queue_matrix = []
+    for line in stack_str:
+        tmp_matrix.append( [line[x] for x in range(1,len(line), 4)] )
+    debug(f"tmp_matrix: {tmp_matrix}")
 
-def f2(line):
-    # TODO
-    return 0
+    for i in range(len(tmp_matrix[0])):
+        tmp_list = [row[i] for row in tmp_matrix]
+        queue_matrix.append(list(filter(lambda item: item != ' ', tmp_list)))
+    debug(f"queue_matrix: {queue_matrix}")
+    return queue_matrix
+
+def process_action(stack, quantity, source_queue, destination_queue):
+    debug(f"stack before: {stack}")
+    sq = deque(stack[source_queue-1])
+    dq = deque(stack[destination_queue-1])
+    for item in range(quantity):
+        dq.appendleft(sq.popleft())
+    stack[source_queue-1] = list(sq)
+    stack[destination_queue-1] = list(dq)
+    debug(f"stack after: {stack}")
+    return stack
+
+def crate_mover_9001(stack, quantity, source_queue, destination_queue):
+    debug(f"stack before: {stack}")
+    sq = deque(stack[source_queue-1])
+    dq = deque(stack[destination_queue-1])
+    tmp_queue = deque([])
+    for item in range(quantity):
+        tmp_queue.appendleft(sq.popleft())
+    debug(f"tmp_queue: {tmp_queue}")
+    for item in tmp_queue:
+        dq.appendleft(item)
+    stack[source_queue-1] = list(sq)
+    stack[destination_queue-1] = list(dq)
+    debug(f"stack after: {stack}")
+    return stack
+
+def process_action_lines(stack, actions_str):
+    
+    for action in actions_str:
+        tmp_action_list=action.split(" ")
+        debug(tmp_action_list)
+        stack = crate_mover_9001(stack,
+            int(tmp_action_list[1]), #quantity
+            int(tmp_action_list[3]), #source_queue
+            int(tmp_action_list[5])) #destination_queue
+    return stack
 
 def get_total(file_name):
-    total = 0
-    
+    stack_str = []
+    actions_str = []
+    is_stack = True
     input_file = open(file_name, 'r')
     for line in input_file:
-        total += f2(line.strip())
+        ls = line.strip()
+        if ls == '' :
+            is_stack = False
+            stack_str.pop()
+            continue
+        
+        if is_stack :
+            stack_str.append(line.replace("\n", ""))
+        else :
+            actions_str.append(ls)
     input_file.close()
-
-    return total
+    stack = process_stack_lines(stack_str)
+    ordered_stack = process_action_lines(stack, actions_str)
+    debug(stack_str)
+    debug(actions_str)
+    debug(ordered_stack)
+    result = ""
+    for queue in ordered_stack:
+        result = f"{result}{queue[0]}"
+    return result
 
 ### TESTS
 
-test1_result = f2('test_value')
-test1_expected_result = True
+test1_result = process_stack_lines([
+    '    [D]    ',
+    '[N] [C]    ',
+    '[Z] [M] [P]'
+])
+test1_expected_result = [
+    ['N', 'Z'],
+    ['D', 'C', 'M'],
+    ['P']]
 if test1_result != test1_expected_result:
     raise Exception(f"TEST_1 FAILED: Expected value: {test1_expected_result}, got: {test1_result}")
 
-test2_result = f2('test_value')
-test2_expected_result = True
+test2_result = crate_mover_9001(
+    [
+    ['N', 'Z'],
+    ['D', 'C', 'M'],
+    ['P']],
+    2,
+    2,
+    3
+    )
+test2_expected_result = [
+    ['N','Z'],
+    ['M'],
+    ['D','C','P']]
 if test2_result != test2_expected_result:
     raise Exception(f"TEST_2 FAILED: Expected value: {test2_expected_result}, got: {test2_result}")
 
 test_result = get_total("input_test")
-expected_result = 2
+expected_result = 'MCD'
 if test_result != expected_result:
     raise Exception(f"INPUT TEST FAILED: Expected value: {expected_result}, got: {test_result}")
 
