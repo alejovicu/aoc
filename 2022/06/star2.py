193c26921704ba007ebfbe96d33ec0529a9cf1c4
Alejandro Rosero Vicuña <alejandroroserov@gmail.com>
2022-12-10 12:28:09 +0100

    day6: star2

--- 2022/06/star2.py
+++ 2022/06/star2.py
@@ -1,15 +1,20 @@
 def debug(log_message):
     import datetime;
     debug_enabled = False
+    # debug_enabled = True
     if debug_enabled:
         print(f"[{datetime.datetime.now()}] - [DEBUG] - {log_message}")
 
-def f1():
-    # TODO
-    return 0
-
-def f2(line):
-    # TODO
+def detects_start_of_packet_marker(text):
+    for i in range(len(text)):
+        marker_length =14
+        marker_str = text[i:i+marker_length]
+        marker_set = set([ char for char in marker_str])
+        debug(f"i: {i}")
+        debug(f"marker_str: {marker_str}")
+        debug(f"marker_set: {marker_set}")
+        if len(marker_str) == len(marker_set):
+            return text.find(marker_str)+marker_length
     return 0
 
 def get_total(file_name):
@@ -17,25 +22,41 @@ def get_total(file_name):
     
     input_file = open(file_name, 'r')
     for line in input_file:
-        total += f2(line.strip())
+        total += detects_start_of_packet_marker(line.strip())
     input_file.close()
 
     return total
 
+
 ### TESTS
+test_result = detects_start_of_packet_marker('mjqjpqmgbljsphdztnvjfqwrcgsmlb')
+test_expected_result = 19
+if test_result != test_expected_result:
+    raise Exception(f"TEST_0 FAILED: Expected value: {test_expected_result}, got: {test_result}")
 
-test1_result = f2('test_value')
-test1_expected_result = True
-if test1_result != test1_expected_result:
-    raise Exception(f"TEST_1 FAILED: Expected value: {test1_expected_result}, got: {test1_result}")
+test_result = detects_start_of_packet_marker('bvwbjplbgvbhsrlpgdmjqwftvncz')
+test_expected_result = 23
+if test_result != test_expected_result:
+    raise Exception(f"TEST_1 FAILED: Expected value: {test_expected_result}, got: {test_result}")
 
-test2_result = f2('test_value')
-test2_expected_result = True
-if test2_result != test2_expected_result:
-    raise Exception(f"TEST_2 FAILED: Expected value: {test2_expected_result}, got: {test2_result}")
+test_result = detects_start_of_packet_marker('nppdvjthqldpwncqszvftbrmjlhg')
+test_expected_result = 23
+if test_result != test_expected_result:
+    raise Exception(f"TEST_2 FAILED: Expected value: {test_expected_result}, got: {test_result}")
+
+test_result = detects_start_of_packet_marker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg')
+test_expected_result = 29
+if test_result != test_expected_result:
+    raise Exception(f"TEST_3 FAILED: Expected value: {test_expected_result}, got: {test_result}")
+
+test_result = detects_start_of_packet_marker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw')
+test_expected_result = 26
+if test_result != test_expected_result:
+    raise Exception(f"TEST_4 FAILED: Expected value: {test_expected_result}, got: {test_result}")
+
 
 test_result = get_total("input_test")
-expected_result = 2
+expected_result = 19
 if test_result != expected_result:
     raise Exception(f"INPUT TEST FAILED: Expected value: {expected_result}, got: {test_result}")
 
